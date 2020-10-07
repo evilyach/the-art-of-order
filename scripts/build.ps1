feb8e09771d53bdd3cf5d47d4db4b4524f54d3cc
Ilya Chesalin <ilya.chesalin@gmail.com>
2020-10-07 12:18:01 +0300

    Added powershell script to build on Windows

---
+++ scripts/build.ps1
@@ -0,0 +1,15 @@
+# Creating virtual environment
+python.exe -m venv .env
+
+./.env/scripts/activate.ps1
+
+# Get-Location
+
+# # Installing system dependencies
+.\.env\Scripts\python.exe -m pip install "pip==20.2.3" "poetry==1.1.0"
+
+# # Installing dependencies specified in pyproject.toml file
+poetry install
+
+# # Post-install handling
+deactivate
