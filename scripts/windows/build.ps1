f5f0093151c9700a3d9d25b03440f9cc6d4bd73e
Ilya Chesalin <ilya.chesalin@gmail.com>
2020-10-07 14:18:21 +0300

    Add dangling lines

--- scripts/windows/build.ps1
+++ scripts/windows/build.ps1
@@ -1,7 +1,11 @@
 ./.env/scripts/activate.ps1
 
+$date = Get-Date -Format "yyyyMMddHHmmss"
+$distpath = '.\dist\windows\build_' + $date
+
 pyinstaller --log-level=WARN `
 	--workpath .build `
+	--distpath $distpath `
 	--noconfirm `
 	--onefile `
 	tao.spec
