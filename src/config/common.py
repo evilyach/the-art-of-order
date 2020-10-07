283729dd120c8e85159b4120d8ebbe004c1a778d
Ilya Chesalin <ilya.chesalin@gmail.com>
2020-10-07 14:18:48 +0300

    Add basic game loop

---
+++ src/config/common.py
@@ -0,0 +1,3 @@
+Settings = {
+    "fps": 60
+}
--- src/tao.py
+++ src/tao.py
@@ -1,10 +1,27 @@
 import pygame
+import pygame.locals
+import sys
+from config.textures import Textures
+from config.common import Settings
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((128, 98))
+    clock = pygame.time.Clock()
+
+    screen = pygame.display.set_mode((800, 600))
     screen.fill((255, 255, 255))
+
+    done = False
+
+    while not done:
+        clock.tick(Settings["fps"])
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        pygame.display.update()
 
 
 if __name__ == "__main__":
