423a5c0f7a3d944ac74a97f1d65ca5cf2bcf8e38
Ilya Chesalin <ilya.chesalin@gmail.com>
2020-10-13 14:22:26 +0300

    Added Logging module

---
+++ src/framework/logger.py
@@ -0,0 +1,72 @@
+import inspect
+import logging
+import os
+
+import colorlog
+
+
+class Logger:
+    def __init__(self, level=logging.DEBUG):
+        self.level = level
+        self.handler = colorlog.StreamHandler()
+        self.format = "%(asctime)s: %(log_color)s%(message)s%(reset)s"
+        self.datefmt = "%H:%M:%S %d/%m"
+        self.log_colors = {
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",
+        }
+
+        self.stream = logging.StreamHandler()
+        self.stream.setLevel(self.level)
+
+        self.formatter = colorlog.ColoredFormatter(
+            self.format, datefmt=self.datefmt, log_colors=self.log_colors
+        )
+        self.stream.setFormatter(self.formatter)
+
+        self.logger = logging.getLogger("root")
+        self.logger.setLevel(self.level)
+        self.logger.addHandler(self.stream)
+
+        self.print_format = "{} ({}:{}) - {}"
+
+    @staticmethod
+    def get_call_info():
+        stack = inspect.stack()
+
+        filename = os.path.normpath(stack[2][1])
+        caller = stack[2][3]
+        line = stack[2][2]
+
+        return filename, caller, line
+
+    def debug(self, message, *args, **kwargs):
+        message = self.print_format.format(*self.get_call_info(), message)
+        self.logger.debug(
+            message.format(self.caller, self.line, self.filename),
+            *args,
+            **kwargs,
+            stack_info=True
+        )
+
+    def info(self, message, *args, **kwargs):
+        message = self.print_format.format(*self.get_call_info(), message)
+        self.logger.info(message, *args, **kwargs)
+
+    def warning(self, message, *args, **kwargs):
+        message = self.print_format.format(*self.get_call_info(), message)
+        self.logger.warning(message, *args, **kwargs)
+
+    def error(self, message, *args, **kwargs):
+        message = self.print_format.format(*self.get_call_info(), message)
+        self.logger.error(message, *args, **kwargs)
+
+    def critical(self, message, *args, **kwargs):
+        message = self.print_format.format(*self.get_call_info(), message)
+        self.logger.critical(message, *args, **kwargs)
+
+
+logger = Logger()
