9c369299600e2f4165c2cad18be2c63ef83fd315
Ilya Chesalin <ilya.chesalin@gmail.com>
2020-10-16 19:33:44 +0300

    Add smooth movement
    Redid the way movement works to not be discrete and block by block, but
    move 1/100 of a block.
    
    See #12

--- src/player/__init__.py
+++ src/player/__init__.py
@@ -2,6 +2,7 @@ import pygame
 
 import config.colors as colors
 import config.settings as settings
+import config.player as player_settings
 from framework.logger import logger
 
 
@@ -11,8 +12,10 @@ class Player(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self, self.groups)
 
         self.game = game
-        self.x = x
-        self.y = y
+        self.x = x * settings.TILESIZE
+        self.y = y * settings.TILESIZE
+        self.vx = 0
+        self.vy = 0
 
         self.image = pygame.Surface((settings.TILESIZE, settings.TILESIZE))
         self.image.fill(colors.YELLOW)
@@ -20,18 +23,59 @@ class Player(pygame.sprite.Sprite):
 
         logger.info("Created player at ({}, {})".format(self.x, self.y))
 
-    def move(self, dx=0, dy=0):
-        if not self.collide(dx, dy):
-            self.x += dx
-            self.y += dy
+    def __get_keys(self):
+        self.vx = 0
+        self.vy = 0
 
-    def collide(self, dx=0, dy=0):
-        for wall in self.game.walls:
-            if wall.x == self.x + dx and wall.y == self.y + dy:
-                return True
+        keys = pygame.key.get_pressed()
 
-        return False
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.vx = -player_settings.PLAYER_SPEED
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.vx = player_settings.PLAYER_SPEED
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.vy = -player_settings.PLAYER_SPEED
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.vy = player_settings.PLAYER_SPEED
+
+        # If moving diagonally, multiply velocity for both coordinates by
+        # $\frac{1}{\sqrt{2}}$, or 0.7071
+        if self.vx != 0 and self.vy != 0:
+            self.vx *= 0.7071
+            self.vy *= 0.7071
+
+    def collide(self, direction):
+        if direction == 'x':
+            hits = pygame.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vx > 0:
+                    self.x = hits[0].rect.left - self.rect.width
+
+                if self.vx < 0:
+                    self.x = hits[0].rect.right
+
+                self.vx = 0
+                self.rect.x = self.x
+
+        if direction == 'y':
+            hits = pygame.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vy > 0:
+                    self.y = hits[0].rect.top - self.rect.height
+
+                if self.vy < 0:
+                    self.y = hits[0].rect.bottom
+
+                self.vy = 0
+                self.rect.y = self.y
 
     def update(self):
-        self.rect.x = self.x * settings.TILESIZE
-        self.rect.y = self.y * settings.TILESIZE
+        self.__get_keys()
+
+        self.x += self.vx * self.game.dt
+        self.y += self.vy * self.game.dt
+
+        self.rect.x = self.x
+        self.collide('x')
+        self.rect.y = self.y
+        self.collide('y')
